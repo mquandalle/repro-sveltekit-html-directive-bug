87d82049c284b93828f14c4b3ef674472542c094
Maxime Quandalle <maxime@quandalle.com>
2021-08-09 15:30:38 +0200

    Create bug reproduction

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+  let exampleText = "Hello world";
+  // Note: it works if we use
+  // exampleText = "<span>Hello world</span>"
+</script>
+
+<h1>Bug reproduction</h1>
+<p>We expect the `exampleText` to be displayed below:</p>
+<p>
+  {#if false}hello{/if}
+  {#if false}hello{/if}
+  {@html exampleText}
+</p>
